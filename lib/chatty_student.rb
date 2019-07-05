fd503b9431451462d73f70fec33373a7aa41f52b
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-05 20:57:58 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,6 +8,24 @@ class ChattyStudent < Student
   def raise_hand
     super
     puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
+    super
+    puts "Pick me!"
   end
 
 end
